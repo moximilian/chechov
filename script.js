4a2ad81b36d998fa58360e5ba0fdfd8c64214238
Максим Сыров <m.syrov@it-bastion.com>
2024-10-07 20:51:09 +0300

    Created web pages successfully!

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
